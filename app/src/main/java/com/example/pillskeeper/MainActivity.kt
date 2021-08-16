99283edd351dabf872f7a7bdac22666110376082
MarvinTM62 <gabrieleoddo6298italia@gmail.com>
2021-08-16 15:11:36 +0200

    google maps

--- app/src/main/java/com/example/pillskeeper/MainActivity.kt
+++ app/src/main/java/com/example/pillskeeper/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.pillskeeper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        }
+
     }
-}
+
