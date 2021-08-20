832cd1b2e8a4d70c469563fe2276be3e66acce56
DavideAltieri <altieri.davide.98@gmail.com>
2021-08-20 15:30:18 +0200

    bottomnavigationmenu

--- app/src/main/java/com/example/pillskeeper/MainActivity.kt
+++ app/src/main/java/com/example/pillskeeper/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.pillskeeper
 
+import android.content.ClipData
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.Sampler
+import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.LinearLayout
+import androidx.appcompat.view.menu.MenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -25,16 +34,72 @@ class MainActivity : AppCompatActivity() {
 
         //var moment:Pill = Pill("moment", "immagine")
         //myRef.child("davide").push().setValue(moment)
+        myRef.child("davide").child("pills").child("momentact").setValue(Pill("momentact", "immagine"))
+
+        myRef.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.hasChild("davide"))
+                    println("ESATTOOOOOOOOOOO")
+                else
+                    println("MA CHE STAI A DI'")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+        myRef.child("davide").child("pills").child("momentact").addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()) {
+                    var sd: String = (snapshot.getValue().toString())
+                    sd = sd.substring(1, sd.length-1)
+                    println(sd)
+
+                    // for(snapshot2 in snapshot.children())
+
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+
 
         val buttonLayoutInv: LinearLayout = findViewById(R.id.layoutInv)
         val buttonLayoutNotify: LinearLayout = findViewById(R.id.layoutNotify)
         val buttonLayoutContact: LinearLayout = findViewById(R.id.layoutContact)
         val buttonLayoutMap: LinearLayout = findViewById(R.id.layoutMap)
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomMenu)
 
+        bottomNavigation.setOnNavigationItemSelectedListener(object : BottomNavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when(item.itemId) {
+                    R.id.menunav -> println(1)
+                    R.id.emailmenu -> println(2)
+                    R.id.menusettings -> println(3)
+                }
+                return true
+            }
+        })
+
+
+
+        /* buttonSettings.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                setContentView(R.layout.activity_login)
+            }
+
+
+        }) */
 
         buttonLayoutInv.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View?) {
-                setContentView(R.layout.activity_login)
+                println("bottone 1")
             }
 
 
